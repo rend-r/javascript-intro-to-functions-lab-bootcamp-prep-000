b460cd2ecc2c8024a1e0c52f4cbcca8eecee7a73
G G <georgiagray7@comcast.net>
2020-09-06 05:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function logWhisper(string) {
   console.log("string")
 }
 function sayHiToGrandma(string) {
-  if string === "I love you, Grandma.";
-  return console.log("I love you, too.")
- else if string === string.toUpperCase();
- return console.log("I can't hear you!")
- else if string === string.toLowerCase();
-  return console.log("YES INDEED!")
+  if string = "I love you, Grandma.";
+  return console.log("I love you, too.");
+ else if string = string.toUpperCase();
+ return console.log("I can't hear you!");
+ else if string = string.toLowerCase();
+  return console.log("YES INDEED!");
 
 }
