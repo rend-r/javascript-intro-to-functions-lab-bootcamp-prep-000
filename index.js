62270f86b1dcaf15f563f75b3c193ac1acf93016
G G <georgiagray7@comcast.net>
2020-09-06 05:51:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  .toUpperCase()
+  console.log("string")
+}
+function logWhisper(string) {
+  ''
+  
+}
+function sayHiToGrandma(string) {
+  if string === "I love you, Grandma.";
+  return console.log("I love you, too.")
+ else if string === string.toUpperCase();
+ return console.log("I can't hear you!")
+ else if string === string.toLowerCase();
+  return console.log("YES INDEED!")
+
+}
